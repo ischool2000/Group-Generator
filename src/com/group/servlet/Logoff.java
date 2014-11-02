127b5fd7462f022101ebd4f76e94a6cce8e40321
LIM61 <LIM61@pitt.edu>
2014-11-02 15:53:23 -0500

    LogOff

--- src/com/group/servlet/Logoff.java
+++ src/com/group/servlet/Logoff.java
@@ -1,11 +1,14 @@
 package com.group.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
 
 /**
  * Servlet implementation class Logoff
@@ -26,12 +29,17 @@ public class Logoff extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response, Object professorHelper) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
 		response.setHeader("Cache-Control", "no-cache, no-store");
 		response.setHeader("Pragma", "no-cache");
-
-		request.getSession().invalidate();
+		
+		Boolean flag = true;
+		JSONObject object = new JSONObject();
+		object.element("flag", flag);
+		
+	    request.getSession().invalidate();
     	response.sendRedirect(request.getContextPath() + "/login.jsp");
 	}
 
