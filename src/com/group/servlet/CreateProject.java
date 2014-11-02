bcf6bc1c5cc6ed17916d2138d0ced42cdc60804c
LIM61 <LIM61@pitt.edu>
2014-11-02 11:49:55 -0500

    NewProject

--- src/com/group/servlet/CreateProject.java
+++ src/com/group/servlet/CreateProject.java
@@ -1,8 +1,7 @@
 package com.group.servlet;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,22 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.group.datahelper.ClassHelper;
-
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
+import com.group.datahelper.SkillHelper;
+import com.group.model.Skill;
 /**
- * Servlet implementation class CreateProject
+ * Servlet implementation class Register
  */
-@WebServlet("/Create_Project")
+@WebServlet("/Student")
 public class CreateProject extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	private static ClassHelper classHelper = new ClassHelper();   
+    private ProjectHelper projectHelper = new ProjectHelper();   
     /**
      * @see HttpServlet#HttpServlet()
      */
-   
     public CreateProject() {
         super();
         // TODO Auto-generated constructor stub
@@ -34,22 +32,27 @@ public class CreateProject extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-    	String name = (String) request.getParameter("name");
-    	int skillId= (Integer) request.getSession().getAttribute("skillId");
-        
-        Boolean flag = classHelper.createProject(name, skillId);
-		
+		List projectList = projectHelper.getAllProject();
+		// TODO Auto-generated method stub
 		JSONArray array = new JSONArray();
-		array.add(value);
+    	for(int i = 0;i < projectList.size();i++){
+    		Project project = (Project) projectList.get(i);
+    		JSONObject projectObject = new JSONObject();
+    		projectObject.element("name", project.getName());
+    		projectObject.element("gender", project.Gender());
+    		projectObject.element("email", project.getEmail());
+    		array.add(projectObject);
+    	}
+        
+
 		
 		response.setContentType("text/json");
 		response.setCharacterEncoding("UTF-8");
 		response.setHeader("Cache-Control", "no-cache");
 		
-		response.getWriter().write(array.toString());
+		response.getWriter().write(object.toString());
 	}
 
 	/**
@@ -57,8 +60,10 @@ public class CreateProject extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		
 	}
-
+		
+	
 }
-
   
