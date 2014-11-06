28f280fd2ab275ff113080fe2b1a08327d6661da
chanlen <buptca@gmail.com>
2014-11-06 15:14:07 -0500

    add Filter to change json to jsonp

--- src/com/group/filter/JsonpFilter.java
+++ src/com/group/filter/JsonpFilter.java
@@ -1,5 +1,36 @@
 package com.group.filter;
 
-public class JsonpFilter {
+import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class JsonpFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		response.getWriter().write("jsonp(");
+		chain.doFilter(request, response);
+		response.getWriter().write(");");
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
