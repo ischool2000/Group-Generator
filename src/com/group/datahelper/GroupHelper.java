b40d63f10dc62d62838dd6ab2469ba69de6a5a77
chanlen <buptca@gmail.com>
2014-11-08 12:16:41 -0500

    change filter

--- src/com/group/datahelper/GroupHelper.java
+++ src/com/group/datahelper/GroupHelper.java
@@ -2,10 +2,14 @@ package com.group.datahelper;
 
 import java.util.List;
 
+import com.group.model.Student;
+
 public class GroupHelper {
 
-	public List createGroup(List<List<Integer>> list) {
+	public List<Student> createGroup(List<List<Integer>> list) {
 		// TODO Auto-generated method stub
+		
+		
 		return null;
 	}
 
