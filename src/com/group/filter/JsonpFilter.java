53d974eeea0fb3b6f5fec905045703a563289209
chanlen <buptca@gmail.com>
2014-11-09 15:32:48 -0500

    change filter

--- src/com/group/filter/JsonpFilter.java
+++ src/com/group/filter/JsonpFilter.java
@@ -23,7 +23,7 @@ public class JsonpFilter implements Filter{
 		// TODO Auto-generated method stub
 		if(request.getParameter("callback") != null){
 			response.getWriter().write(request.getParameter("callback") + "(");
-			
+			chain.doFilter(request, response);
 			response.getWriter().write(");");
 		}else{
 			chain.doFilter(request, response);
